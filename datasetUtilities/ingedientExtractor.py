93a9d35c2193178686f262985eca23b16cee7b7d
aiacovazzi <ar.iacovazzi@gmail.com>
2024-11-18 22:16:24 +0100

    updated dataset utilities

--- datasetUtilities/ingedientExtractor.py
+++ datasetUtilities/ingedientExtractor.py
@@ -1,23 +1,41 @@
 import pandas as pd
-df = pd.read_csv('data\\sample\\cleanedSampleOf100RecipesHeaSE.csv', quotechar='"')
+df = pd.read_csv('datasetUtilities\data\cleaned_final_recipes_set.csv', quotechar='"')
 
-#define an array to store the ingredients
-ingredients = []
+#define an hasmap to store the ingredients along with the id of the recipe they belong to
+ingredients = {}
 
-for i in df['ingredients']:
-    for ingredient in i.split(','):
+#loop over the df and extract the ingredients
+for index, row in df.iterrows():
+    #get the ingredients of the recipe
+    for ingredient in row['ingredients'].split(','):
         #remove the characters '[', ']' and ''' from the ingredient
-        ingredient = ingredient.replace('[','').replace(']','').replace('\'','')
+        ingredient = ingredient.replace('[','').replace(']','').replace('\'','').replace('\"','')
         #remove the characters '_' and everithing after it
         ingredient = ingredient.split('_')[0] 
         #remove trailing and leading whitespaces
         ingredient = ingredient.strip()
-        #add the ingredient to the array
-        ingredients.append(ingredient)
+        #add the ingredient to the hashmap
+        if ingredient in ingredients:
+            ingredients[ingredient].append(row['recipe_id'])
+        else:
+            ingredients[ingredient] = [row['recipe_id']]
 
-print(len(ingredients))
+#order the ingredients by the number of recipes they appear in
+ingredients = dict(sorted(ingredients.items(), key=lambda item: len(item[1]), reverse=True))
+
+df = pd.DataFrame(list(ingredients.items()), columns=['ingredient', 'recipe_id'])
+df.to_csv('datasetUtilities\data\ingredients.csv', index=False)
+
+
+
 
 #transform the array into an hash table that counts the number of times each ingredient appears
-from collections import Counter
+'''from collections import Counter
 ingredientCounter = Counter(ingredients)
+#order the hash table by the number of appearances
+ingredientCounter = dict(sorted(ingredientCounter.items(), key=lambda item: item[1], reverse=True))
 print(ingredientCounter)
+
+#save the hash table into a csv file
+df = pd.DataFrame(list(ingredientCounter.items()), columns=['ingredient', 'count'])
+df.to_csv('datasetUtilities\\data\\ingredients.csv', index=False)'''
